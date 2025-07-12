7e0e64985ee85a699bd51d671770973f0003e6c9
avighna-tripathi <avighnatripathi1102@gmail.com>
2025-07-12 15:31:55 +0530

    Updated

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -10,13 +10,13 @@ const generateToken = (id) => {
 // @desc    Register user
 exports.registerUser = async (req, res) => {
   const { name, email, password } = req.body;
-  const role = email === 'nehetevedant9@gmail.com' ? 'admin' : 'user';
+  const role = email === 'nehetevedant001@gmail.com' ? 'admin' : 'user';
 
   try {
     const userExists = await User.findOne({ email });
     if (userExists) return res.status(400).json({ message: 'User already exists' });
 
-    const user = await User.create({ name, email, password });
+    const user = await User.create({ name, email, password, role });
 
     res.status(201).json({
       _id: user._id,
